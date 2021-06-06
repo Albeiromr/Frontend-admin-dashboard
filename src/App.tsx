941451eb21a2d351de980a3322d2fce4a9915023
Albeiro Marin <xlandru@hotmail.com>
2021-06-06 11:19:49 -0500

    Delete  unnecessary context

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router} from "react-router-dom";
 import MainContainer from './routes/MainContainer/MainContainer';
-import MainNavContextProvider from './context/Main nav context/MainNavContext';
-import ProductGridContextProvider from './context/Product grid context/ProductGridContext';
-import ProductDetailsContextProvider from './context/Product details context/ProductDetailsContext';
-import ShoppingCartContextProvider from './context/Shopping cart context/ShoppingCartContext';
 import AdminDashboardContextProvider from './context/Admin dashboard context/AdminDashboardContext';
 
 
@@ -15,18 +11,12 @@ function App() {
     <Router >
 
     <div className="App">
+      
       <AdminDashboardContextProvider>
-      <ShoppingCartContextProvider>
-      <ProductDetailsContextProvider>
-      <ProductGridContextProvider>
-      <MainNavContextProvider>
 
       <MainContainer />
 
-      </MainNavContextProvider>
-      </ProductGridContextProvider>
-      </ProductDetailsContextProvider>
-      </ShoppingCartContextProvider>
+     
       </AdminDashboardContextProvider>
       
     </div>
