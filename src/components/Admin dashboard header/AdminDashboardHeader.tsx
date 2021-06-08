77e39853a4a92df942dd76c081fddee87e854ffd
Albeiro Marin <xlandru@hotmail.com>
2021-06-08 11:29:31 -0500

    Delete unnecessary footer icons and set profile picture

--- src/components/Admin dashboard header/AdminDashboardHeader.tsx
+++ src/components/Admin dashboard header/AdminDashboardHeader.tsx
@@ -2,6 +2,7 @@ import React, {useState, useRef, useContext} from 'react';
 import "./0px-599px.scss";
 import "./600px-1024px.scss";
 import "./1025px-1920px.scss";
+import profile from '../../assets/images/profile.jpg';
 import graySearch from '../../assets/icons/graySearch.svg';
 import validator from 'validator';
 import {AdminDashboardContext} from '../../context/Admin dashboard context/AdminDashboardContext';
@@ -48,7 +49,7 @@ const AdminDashboardHeader = () => {
                     searchBar.current!.style.border = "1px #D1D1D1 solid";
                 }, 8000);
 
-            }
+            };
         };
     };
     
@@ -98,11 +99,11 @@ const AdminDashboardHeader = () => {
 
             <div className="admin-dashboard-header__admin-container">
                 <div className="admin-dashboard-header__admin-image-container">
-                    {/* <img className="admin-dashboard-header__admin-image" src={test} alt="admin" /> */}
+                    <img className="admin-dashboard-header__admin-image" src={profile} alt="admin" />
                 </div>
                 <div className="admin-dashboard-header__admin-line"/>
                 <div className="admin-dashboard-header__admin-text-container">
-                    <p className="admin-dashboard-header__admin-name">Andru Dexter</p>
+                    <p className="admin-dashboard-header__admin-name">Albeiro Marín</p>
                     <p className="admin-dashboard-header__admin-roll">Admin</p>
                 </div>
             </div>
